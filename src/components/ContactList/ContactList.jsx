67454df5abcb27976e49e60c2ee2de261d119476
NataOz <merlincat.no@gmail.com>
2024-03-03 22:03:51 +0200

    add logic and styles

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,14 @@
 import Contact from '../Contact/Contact';
-const ContactList = () => {
+import css from './ContactList.module.css';
+const ContactList = ({ visibleContcats, onDelete }) => {
   return (
-    <>
-      <Contact />
-    </>
+    <ul className={css.contactsList}>
+      {visibleContcats.map(contact => (
+        <li key={contact.id} className={css.contactListItem}>
+          <Contact data={contact} onDelete={onDelete} />
+        </li>
+      ))}
+    </ul>
   );
 };
 export default ContactList;
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,4 +1,15 @@
-const SearchBox = () => {
-  return <></>;
+import css from './SearchBox.module.css';
+const SearchBox = ({ value, onSearch }) => {
+  return (
+    <div className={css.searchWrapper}>
+      <p>Find contacts by name</p>
+      <input
+        className={css.searchInput}
+        type="text"
+        value={value}
+        onChange={e => onSearch(e.target.value)}
+      />
+    </div>
+  );
 };
 export default SearchBox;
